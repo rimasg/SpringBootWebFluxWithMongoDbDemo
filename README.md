958f083a8260b8ec4226877d7abaeb362d5369b7
Rimas G <rimasg@gmail.com>
2022-06-30 11:40:53 +0300

    Added README.md

---
+++ README.md
@@ -0,0 +1,6 @@
+# Spring Boot WebFlux with MongoDb Demo
+
+## Configure and run the project
+1. Install and set up MongoDB with the params defined in the `application.properties` file
+2. Run `./gradlew bootRun`
+3. Go to url http://localhost:8080/greeting
--- src/main/java/com/sid/demo/SpringBootDemoApplication.java
+++ src/main/java/com/sid/demo/SpringBootDemoApplication.java
@@ -2,10 +2,10 @@ package com.sid.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
 
 @SpringBootApplication
-@EnableMongoRepositories
+@EnableReactiveMongoRepositories
 public class SpringBootDemoApplication {
 
 	public static void main(String[] args) {
